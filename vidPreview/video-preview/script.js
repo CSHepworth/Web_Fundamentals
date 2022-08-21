466cf3dcfa4647b206f8db344128f2482fb405d0
CSHepworth <109162129+CSHepworth@users.noreply.github.com>
2022-08-20 22:48:39 -0600

    video

---
+++ vidPreview/video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+var x = document.getElementById("video")
+
+function over(element) {
+    x.play("video.mp4")
+}
+
+function out(element) {
+    x.pause("video.mp4");
+}
