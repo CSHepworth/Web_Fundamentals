bc525e0ac21e35d3b74c11b18f473279c8d04e0b
CSHepworth <109162129+CSHepworth@users.noreply.github.com>
2022-08-22 22:25:23 -0600

    javascript

--- likes2/likes2.js
+++ likes2/likes2.js
@@ -1,6 +1,4 @@
 function addLike(element) {
-    var l = document.querySelector(element).innerHTML;
-    l = parseInt(l, 10);
-    l++;
-    document.querySelector(element).innerHTML = l;
+    var like = document.querySelector(element);
+    like.innerHTML = parseInt(like.innerHTML, 10) + 1;
 }
---
+++ profilePage/profilePage.js
@@ -0,0 +1,7 @@
+function editProf(element) {
+    document.querySelector(element).innerHTML = "Ferris Bueller";
+}
+
+function removeReq(element) {
+    document.querySelector(element).remove();
+}
